551f9e1fec31e9b2021203f7617ecf789aaadc1c
Vladislav Serdyukov <pandaisback62@mail.ru>
2024-10-28 17:27:23 +0300

    remove httpAgent

--- refreshToken.js
+++ refreshToken.js
@@ -1,14 +1,7 @@
 const axios = require("axios");
-const https = require("https");
-
-
-const agent = new https.Agent({
-    rejectUnauthorized: false,
-  });
 
 async function refreshToken() {
-    const response = await axios.post('https://ngw.devices.sberbank.ru:9443/api/v2/oauth',{httpsAgent: agent}, {
-    })
+    const response = await axios.post('https://ngw.devices.sberbank.ru:9443/api/v2/oauth')
     .then((res) => {
         process.env.ACCESS_TOKEN = res.data.access_token
     })
